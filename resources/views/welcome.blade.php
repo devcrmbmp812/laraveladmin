c96f9ace5f379db01d0ce2316ea4598b1f9b3c7a
Ri Cheng <mohdumairdev2017@gmail.com>
2018-11-14 23:46:41 +0800

    new update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -84,6 +84,9 @@
                     Laravel
                 </div>
                 <example-component></example-component>
+                <passport-clients></passport-clients>
+                <passport-authorized-clients></passport-authorized-clients>
+                <passport-personal-access-tokens></passport-personal-access-tokens>
             </div>
         </div>
         <script src="{{ asset('js/app.js') }}"></script>
--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,6 @@ Route::view('/welcome','testwelcome', ['name' => 'martin']);
 Route::get('user/{ppp?}/{id?}', 'ParamsController@index');
 
 
-Auth::routes();
+Auth::routes(['verify' => true]);
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('home')->middleware('verified');
